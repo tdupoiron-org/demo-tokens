6d81aaed8a33704f1c40184cc59853840c126382
Thomas Dupoiron <tdupoiron@github.com>
2023-12-20 10:04:31 +0000

    Update createGitHubReference function

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const octokit = new Octokit({
   auth: process.env.GITHUB_TOKEN,
 });
 
-async function createGitHubReference() {
+async function createGitHubReference(newRef, sha) {
   const response = await octokit.request("POST /repos/{owner}/{repo}/git/refs", {
     owner: "tdupoiron-org",
     repo: "demo-tokens",
-    ref: "refs/heads/main",
-    sha: "827efc6d56897b048c772eb4087f854f46256132",
+    ref: newRef,
+    sha: sha,
   });
 }
+
+createGitHubReference("refs/heads/main", "cc9dc9b7a270fa1ae80ba17ca42101c53c796bdb");
